f75cc4381472a8ddb6ef17ebec2c3d265addc91e
TemidoRocha <manueltemidorocha@gmail.com>
2022-09-25 11:35:12 +0100

    as per github, the youtube series is incomplete

---
+++ slide-button.d.ts
@@ -0,0 +1,12 @@
+import { LitElement } from "lit";
+declare class SlideButton extends LitElement {
+    static styles: import("lit").CSSResult;
+    render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "slide-button": SlideButton;
+    }
+}
+export {};
+//# sourceMappingURL=slide-button.d.ts.map
